44094869373473b18bf88fbdf3fcbf67495962ef
XHJ2501 <1746114767@qq.com>
2021-07-18 17:49:47 +0800

    新增一个爬虫小项目

--- requests_4(开封菜地址).py
+++ requests_4(开封菜地址).py
@@ -5,12 +5,12 @@ url = "http://www.kfc.com.cn/kfccda/ashx/GetStoreList.ashx"
 city_name = input("Please enter a city name:")
 param = {
     'op': 'keyword',
-    'keyword':city_name,
+    'keyword': city_name,
     'cname': '',
     'pid': '',
     'pageIndex': '1',  # 查找结果第几页
     'pageSize': '10'  # 一页显示多少个
-}
+}  # 所有参数都要有
 headers = {
     'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"
 }
