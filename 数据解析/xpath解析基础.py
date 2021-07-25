80e47d1fe85200ac26684d5cd660f42bd8dc4f25
XHJ2501 <1746114767@qq.com>
2021-07-25 21:49:52 +0800

    新增了xpath的数据解析方法

---
+++ 数据解析/xpath解析基础.py
@@ -0,0 +1,13 @@
+from lxml import etree
+
+parser = etree.HTMLParser(encoding='utf-8')  # 使etree不报错
+tree = etree.parse('cosmetic.html', parser=parser)  # 实例化etree对象，且将被解析的源码加载到了该对象中
+# r = tree.xpath('/html/body/div')  # '/’：表示的是从根节点开始定位，表示的是一个层级。
+# r = tree.xpath('/html//div')  # '//’：表示的是多个层级。
+# r = tree.xpath('//div')  # '//’：可以表示从任意位置开始定位。
+# r = tree.xpath('//div[@class="dzpzmain"]')  # 属性定位，tag[@attrName="attrValue"]
+# r = tree.xpath('//div[@class="hzbtabs"]/span[1]')  # 索引定位，索引从1开始
+# r = tree.xpath('//div[@class="hzbtabs"]/span[1]/text()')[0]  # 取文本：/text()标签中直系文本内容
+# r = tree.xpath('//span[1]//text()')  # 取文本：//text()标签中非直系文本内容
+r = tree.xpath('//div[@class="hzbtabs"]/input/@id')  # 去属性：/@attrName
+print(r)
